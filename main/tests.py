bcd4af79308c80474e4057aee81f2e3335d2bdb6
Joe Kawa <joekawa@yahoo.com>
2023-03-28 08:29:08 -0500

    added test script for backend models

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,39 @@
 from django.test import TestCase
-
+from main.models import *
+from django.contrib.auth.models import User
 # Create your tests here.
+
+class ModelTest(TestCase):
+    def test_create_and_save_model(self):
+
+        # *Create test user
+        test_user = User.objects.create(username='test_user',
+                                        password='Password')
+        test_user.save()
+
+        # *Create a new instance of MyModel
+        beer_test = BEER.objects.create(name='test beer',
+                                        description='beer in test script')
+        brewery_test = BREWERY.objects.create(name='test brewery',
+                                              description='brewery in ' +
+                                              'test script')
+
+        favorite_test = FAVORITE.objects.create(user=test_user, beer=beer_test)
+
+        # *Save the model to the database
+        favorite_test.save()
+
+        beer_test.save()
+        brewery_test.save()
+
+        # *Retrieve the saved model from the database
+        beer_save_test = BEER.objects.get(pk=beer_test.pk)
+        brewery_save_test = BREWERY.objects.get(pk=brewery_test.pk)
+
+        favorite_save_test = FAVORITE.objects.get(pk=favorite_test.pk)
+
+        # *Check that the saved model has the correct values
+        self.assertEqual(beer_save_test.name, 'test beer')
+        self.assertEqual(brewery_save_test.name, 'test brewery')
+        self.assertEqual(favorite_save_test.user.username, 'test_user')
+
