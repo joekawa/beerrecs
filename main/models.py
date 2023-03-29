3aeb69ee320f78c0446a340afd68ee2e6c0faa3f
Joe Kawa <joekawa@yahoo.com>
2023-03-28 21:39:27 -0500

    added created by and created date fields

--- main/models.py
+++ main/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 # Create your models here.
 
 
@@ -9,14 +10,19 @@ class BREWERY(models.Model):
     city = models.TextField(max_length=50, null=True)
     state = models.TextField(max_length=50, null=True)
     country = models.TextField(max_length=50, null=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    created_date = models.DateTimeField(default=timezone.now)
 
 
 class BEER(models.Model):
     name = models.TextField(max_length=40)
     description = models.TextField(max_length=500)
     brewery = models.ForeignKey(BREWERY, on_delete=models.CASCADE, null=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    created_date = models.DateTimeField(default=timezone.now)
 
 
 class FAVORITE(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     beer = models.ForeignKey(BEER, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(default=timezone.now)
