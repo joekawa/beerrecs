95e3d297738c624e3442e79e05e07f4a5a8113bf
Joe Kawa <joekawa@yahoo.com>
2023-03-30 08:39:57 -0500

    added profile model

--- main/models.py
+++ main/models.py
@@ -3,12 +3,76 @@ from django.contrib.auth.models import User
 from django.utils import timezone
 # Create your models here.
 
+STATES = (
+    ("AL", "Alabama"),
+    ("AK", "Alaska"),
+    ("AZ", "Arizona"),
+    ("AR", "Arkansas"),
+    ("CA", "California"),
+    ("CO", "Colorado"),
+    ("CT", "Connecticut"),
+    ("DE", "Delaware"),
+    ("FL", "Florida"),
+    ("GA", "Georgia"),
+    ("HI", "Hawaii"),
+    ("ID", "Idaho"),
+    ("IL", "Illinois"),
+    ("IN", "Indiana"),
+    ("IA", "Iowa"),
+    ("KS", "Kansas"),
+    ("KY", "Kentucky"),
+    ("LA", "Louisiana"),
+    ("ME", "Maine"),
+    ("MD", "Maryland"),
+    ("MA", "Massachusetts"),
+    ("MI", "Michigan"),
+    ("MN", "Minnesota"),
+    ("MS", "Mississippi"),
+    ("MO", "Missouri"),
+    ("MT", "Montana"),
+    ("NE", "Nebraska"),
+    ("NV", "Nevada"),
+    ("NH", "New Hampshire"),
+    ("NJ", "New Jersey"),
+    ("NM", "New Mexico"),
+    ("NY", "New York"),
+    ("NC", "North Carolina"),
+    ("ND", "North Dakota"),
+    ("OH", "Ohio"),
+    ("OK", "Oklahoma"),
+    ("OR", "Oregon"),
+    ("PA", "Pennsylvania"),
+    ("RI", "Rhode Island"),
+    ("SC", "South Carolina"),
+    ("SD", "South Dakota"),
+    ("TN", "Tennessee"),
+    ("TX", "Texas"),
+    ("UT", "Utah"),
+    ("VT", "Vermont"),
+    ("VA", "Virginia"),
+    ("WA", "Washington"),
+    ("WV", "West Virginia"),
+    ("WI", "Wisconsin"),
+    ("WY", "Wyoming")
+)
+
+
+class PROFILE(models.Model):
+    user = models.ForeignKey(User, blank=False, null=False,
+                             on_delete=models.CASCADE)
+    city = models.TextField(max_length=25, blank=True)
+    state = models.CharField(choices=STATES, max_length=2, blank=True)
+    zip_code = models.CharField(max_length=5, blank=True)
+    first_name = models.TextField(max_length=20, blank=True)
+    last_name = models.TextField(max_length=20, blank=True)
+    email = models.EmailField(blank=True)
+
 
 class BREWERY(models.Model):
     name = models.TextField(max_length=100)
     description = models.TextField(max_length=500)
     city = models.TextField(max_length=50, null=True)
-    state = models.TextField(max_length=50, null=True)
+    state = models.TextField(choices=STATES, null=True)
     country = models.TextField(max_length=50, null=True)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     created_date = models.DateTimeField(default=timezone.now)
--- main/tests.py
+++ main/tests.py
@@ -20,12 +20,18 @@ class ModelTest(TestCase):
 
         favorite_test = FAVORITE.objects.create(user=test_user, beer=beer_test)
 
+        profile_test = PROFILE.objects.create(user=test_user,
+                                              email='joekawa@yahoo.com',
+                                              city='Omaha',
+                                              state='NE')
+
 
         # *Save the model to the database
         favorite_test.save()
 
         beer_test.save()
         brewery_test.save()
+        profile_test.save()
 
         tag_test = TAG.objects.create(beer=beer_test, tag='first tag',
                                       created_by=test_user)
@@ -37,9 +43,11 @@ class ModelTest(TestCase):
 
         favorite_save_test = FAVORITE.objects.get(pk=favorite_test.pk)
         tag_save_test = TAG.objects.get(pk=tag_test.pk)
+        profile_save_test = PROFILE.objects.get(pk=profile_test.pk)
 
         # *Check that the saved model has the correct values
         self.assertEqual(beer_save_test.name, 'test beer')
         self.assertEqual(brewery_save_test.name, 'test brewery')
         self.assertEqual(favorite_save_test.user.username, 'test_user')
         self.assertEqual(tag_save_test.tag, 'first tag')
+        self.assertEqual(profile_save_test.city, 'Omaha')
